e8213a3eef0b9452dac9a8f2fcf6791c785dfa3e
Joaquin Diaz <joaquin.diaz@ies.cl>
2019-11-28 15:48:50 -0300

    added public test networks

--- networks.js
+++ networks.js
@@ -1,3 +1,8 @@
+require('dotenv').config();
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraProjectId = process.env.INFURA_PROJECT_ID;
+
 module.exports = {
   networks: {
     development: {
@@ -7,6 +12,30 @@ module.exports = {
       gas: 5000000,
       gasPrice: 5e9,
       networkId: '*'
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.DEV_MNEMONIC,
+          'https://ropsten.infura.io/v3/' + infuraProjectId
+        ),
+      networkId: 3 // Ropsten's id
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.DEV_MNEMONIC,
+          'https://rinkeby.infura.io/v3/' + infuraProjectId
+        ),
+      networkId: 4 // Rinkeby's id
+    },
+    kovan: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.DEV_MNEMONIC,
+          'https://kovan.infura.io/v3/' + infuraProjectId
+        ),
+      networkId: 42 // Kovan's id
     }
   }
 };
